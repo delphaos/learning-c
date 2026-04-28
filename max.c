6659d0378dbd21d3cc98af628e523d40bed9e426
delphaos <shaveglobe@yandex.ru>
2026-04-29 00:02:26 +0300

    perfecto!!

--- max.c
+++ max.c
@@ -1,8 +1,27 @@
 #include <stdio.h>
 
-int main() {
+int find_max(int a, int b);  // Объявляем, что такая функция будет дальше
 
-    getchar();
+int main() {
+    int a, b;
+    char c;
+
+    // Нам нужно, чтобы scanf успешно считал 2 числа и 1 символ (пробел или перенос)
+    while (scanf("%d %d%c", &a, &b, &c) != 3 || (c != ' ' && c != '\n')) {
+        printf("n/a\n");
+        while (getchar() != '\n');
+    }
+    printf("%d\n", find_max(a, b));
 
     return 0;
+}
+
+int find_max(int a, int b) {
+    int max = 0;
+    if (a > b) {
+        max = a;
+    } else {
+        max = b;
+    }
+    return max;
 }
